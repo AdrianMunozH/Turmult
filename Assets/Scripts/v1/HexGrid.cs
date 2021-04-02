fcab16c736d46fbe25e57991e5b6f613f5368048
adrian_mh@hotmail.de <adrian_mh@hotmail.de>
2021-04-02 21:22:59 +0200

    erste proto

---
+++ Assets/Scripts/v1/HexGrid.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+    HexCell[] cells;
+    public Text cellLabelPrefab;
+    Canvas gridCanvas;
+    HexMesh hexMesh;
+    MeshCollider meshCollider;
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.blue;
+    void Start () {
+        hexMesh.Triangulate(cells);
+    }
+    void Update () {
+        if (Input.GetMouseButton(0)) {
+            HandleInput();
+        }
+    }
+
+    void HandleInput () {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) {
+            TouchCell(hit.point);
+        }
+    }
+	
+    void TouchCell (Vector3 position) {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        highlightCell(coordinates.X,coordinates.Z,1);
+        Debug.Log("touched at " + coordinates.ToString());
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        //test, cell hat ja kein rendere weil sich die mesh darum kümmert.
+        cell.gameObject.GetComponent<Renderer>().sharedMaterial.SetColor("_Color",Color.blue);
+        //    
+        cell.color = touchedColor;
+        hexMesh.Triangulate(cells);
+    }
+    
+    void Awake () {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++) {
+            for (int x = 0; x < width; x++) {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+	
+    void CreateCell (int x, int z, int i) {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * 10f;
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition =
+            new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+    //test
+    void highlightCell (int x, int z, int i) {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 1f;
+        position.z = z * 10f;
+
+        HexCell cell = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = Color.yellow;
+        cell.color.a = 0.7f;
+    }
+}
---
+++ Assets/Scripts/v2/HGrid.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+    HCell[] cells;
+    public HCell cellPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Awake () {
+        
+        cells = new HCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++) {
+            for (int x = 0; x < width; x++) {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HexMap()
+    {
+        
+    }
+
+    void CreateCell (int x, int z, int i) {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * 1f;
+
+        HCell cell = cells[i] = Instantiate<HCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        /*cell.color = defaultColor;
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition =
+            new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+        */
+    }
+}
