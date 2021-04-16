efc9042ad36efbc069036556593559b62fd953af
Quint Strakeljahn <q.strakeljahn@gmx.de>
2021-04-16 13:37:13 +0200

    Kameraclamping hinzugefügt

--- Assets/Scripts/v2/CameraController.cs
+++ Assets/Scripts/v2/CameraController.cs
@@ -8,6 +8,12 @@ public class CameraController : MonoBehaviour
     public float scrollSpeed = 5f;
     public float minScrollY = 20f;
     public float maxScrollY = 100f;
+    
+    public float clampMinZ = -205f;
+    public float clampMaxZ = 50f;
+    public float clampMinX = -100f;
+    public float clampMaxX = 100f;
+    
 
     public float panBorderThickness = 10;
 
@@ -28,28 +34,39 @@ public class CameraController : MonoBehaviour
         }
         if (!doMovement)
             return;
-        
+
         //Hier die x,y Bewegungen der Kamera
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
-            transform.Translate(Vector3.forward * (cameraSpeed * Time.deltaTime),Space.World);
+            if (transform.position.z <= clampMaxZ)
+            {
+                transform.Translate(Vector3.forward * (cameraSpeed * Time.deltaTime), Space.World);
+            }
         }
         if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
         {
-            transform.Translate(Vector3.left * (cameraSpeed * Time.deltaTime),Space.World);
+            if (transform.position.x >= clampMinX)
+            {
+                transform.Translate(Vector3.left * (cameraSpeed * Time.deltaTime), Space.World);
+            }
         }
         if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
         {
-            transform.Translate(Vector3.back * (cameraSpeed * Time.deltaTime),Space.World);
+            if (transform.position.z >= clampMinZ)
+            {
+                transform.Translate(Vector3.back * (cameraSpeed * Time.deltaTime), Space.World);
+            }
         }
         if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            transform.Translate(Vector3.right * (cameraSpeed * Time.deltaTime),Space.World);
+            if (transform.position.x <= clampMaxX)
+            {
+                transform.Translate(Vector3.right * (cameraSpeed * Time.deltaTime), Space.World);
+            }
         }
         
         // Hier das Scrolling
         float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
-
         Vector3 pos = transform.position;
         pos.y -= scrollWheel * 1000 * scrollSpeed * Time.deltaTime;
         pos.y = Mathf.Clamp(pos.y, minScrollY, maxScrollY);
