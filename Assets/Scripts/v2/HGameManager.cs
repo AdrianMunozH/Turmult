0cb70345dc804083247b03bff341212876cdd12b
Quint Strakeljahn <q.strakeljahn@gmx.de>
2021-04-17 11:26:43 +0200

    Endcell über den Editor verfügbar gemacht.

--- Assets/Scripts/v2/HGameManager.cs
+++ Assets/Scripts/v2/HGameManager.cs
@@ -12,6 +12,9 @@ public class HGameManager : MonoBehaviour
     HCell start;
     HCell end;
 
+    [Header("Setup Endpoint")] public int distanceFromSpawn = 3;
+
+
 
     //test für shortest path
 
@@ -19,7 +22,6 @@ public class HGameManager : MonoBehaviour
 
     private bool deleteLater;
 
-    // Start is called before the first frame update
     void Start()
     {
         // test erstmal nur die eine
@@ -28,87 +30,17 @@ public class HGameManager : MonoBehaviour
         _enemySpawns[1] = Instantiate<EnemySpawn>(spawnPoint);
         _enemySpawns[2] = Instantiate<EnemySpawn>(spawnPoint);
 
-        _enemySpawns[0].end = HGrid.Instance.GetCellIndex(0, -5, 5);
-        _enemySpawns[1].end = HGrid.Instance.GetCellIndex(5, 0, -5);
-        _enemySpawns[2].end = HGrid.Instance.GetCellIndex(-5, 5, 0);
+        _enemySpawns[0].end = HGrid.Instance.GetCellIndex(0, -distanceFromSpawn, distanceFromSpawn);
+        _enemySpawns[1].end = HGrid.Instance.GetCellIndex(distanceFromSpawn, 0, -distanceFromSpawn);
+        _enemySpawns[2].end = HGrid.Instance.GetCellIndex(-distanceFromSpawn, distanceFromSpawn, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && !cooldown)
-        {
-            HandleInput();
-        }
+
     }
-
-    void HandleInput()
-    {
-        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(inputRay, out hit))
-        {
-            //HighlightCell2(hit.collider.gameObject.transform.position);
-            /*
-            Debug.Log(input);
-            if (input == 0)
-            {
-                OnCooldown();
-                input++;
-                start = hit.collider.gameObject.GetComponent<HCell>();
-                Debug.Log("Start" + start.coordinates.ToString());
-                
-            } else if (input == 1)
-            {
-                OnCooldown();
-                end = hit.collider.gameObject.GetComponent<HCell>();
-                Debug.Log("end" + end.coordinates.ToString());
-                input++;
-
-            }
-            */
-            if (hit.collider.gameObject.name == "pseudoButton")
-            {
-                SpawnEnemyWave();
-            }
-        }
-
-        /*
-    void HandleInput () {
-        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        
-        if (Physics.Raycast(inputRay, out hit)) {
-            //HighlightCell2(hit.collider.gameObject.transform.position);
-
-            if (input == 0)
-            {
-                OnCooldown();
-                input++;
-                start = hit.collider.gameObject.GetComponent<HCell>();
-                Debug.Log("Start" + start.coordinates.ToString());
-                spath = Solve(start);
-                
-                
-
-            } else if (input == 1)
-            {
-                OnCooldown();
-                end = hit.collider.gameObject.GetComponent<HCell>();
-                List<HCell> sp = RecPath(start, end, spath);
-                sp = ShortestPath(sp);
-                Debug.Log("***Ergebnis*** " + hGrid.ListToString(sp) + " : "+ sp.Count);
-                input = 0;
-                if(sp.Count > 0)
-                    enemySpawn.SpawnEnemy(hGrid.HCellPositions(sp));
-            }
-            //Neighb(hit.collider.gameObject.GetComponent<HCell>());
-            
-            //TouchCell(hit.point);
-        }
-        */
-    }
+    
+    
 
     private void OnCooldown()
     {
