4061f6badd9fe77077451c7c806c250c39a90973
Quint Strakeljahn <q.strakeljahn@gmx.de>
2021-07-27 17:38:39 +0200

    TUR-3-Error beim Bauen entfernt.

--- Assets/Scripts/Field/HexCoordinates.cs
+++ Assets/Scripts/Field/HexCoordinates.cs
@@ -87,17 +87,4 @@ namespace Field
             return new HexCoordinates(iX, iZ);
         }
     }
-    [CustomPropertyDrawer(typeof(HexCoordinates))]
-    public class HexCoordinatesDrawer : PropertyDrawer {
-        public override void OnGUI (
-            Rect position, SerializedProperty property, GUIContent label
-        ) {
-            HexCoordinates coordinates = new HexCoordinates(
-                property.FindPropertyRelative("x").intValue,
-                property.FindPropertyRelative("z").intValue
-            );
-            position = EditorGUI.PrefixLabel(position, label);
-            GUI.Label(position, coordinates.ToString());
-        }
-    }
 }
