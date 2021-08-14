d443c71c741e2c54f10e976a0f9813370490258d
Quint Strakeljahn <q.strakeljahn@gmx.de>
2021-08-15 01:17:30 +0200

    Sound hinterlegt

--- Assets/Scripts/Audio/AudioChanger.cs
+++ Assets/Scripts/Audio/AudioChanger.cs
@@ -1,33 +1,39 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UIElements;
+using UnityEngine.InputSystem.Switch;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using ValueObjects;
 
 namespace Audio
 {
-    public class AudioChannelMixer : MonoBehaviour
+    public class AudioChanger : MonoBehaviour
     {
-        private String[] mixerGroupVolumeArray = {"Master", "FX","Music"};
-        public String mixerGroup;
+        
+        private String[] mixerGroupVolumeArray = {"MasterVol", "FXVol","MusicVol"};
+
         public AudioMixer mixer;
+        public String mixerGroup; 
+
         public FloatObject master;
         public FloatObject music;
         public FloatObject fx;
 
         private void Awake()
         {
+        
             switch (mixerGroup)
             {
-                case "Music":
+                case "MusicVol":
                     mixer.SetFloat(getMixerGroup(mixerGroup), Mathf.Log10(music.RuntimeValue) * 20);
                     gameObject.GetComponent<Slider>().value = music.RuntimeValue;
                     break;
-                case "FX":
+                case "FXVol":
                     mixer.SetFloat(getMixerGroup(mixerGroup),Mathf.Log10(fx.RuntimeValue) * 20 );
                     gameObject.GetComponent<Slider>().value = fx.RuntimeValue;
                     break;
-                case "Master":
+                case "MasterVol":
                     mixer.SetFloat(getMixerGroup(mixerGroup), Mathf.Log10(master.RuntimeValue) * 20);
                     gameObject.GetComponent<Slider>().value = master.RuntimeValue;
                     break;
@@ -35,16 +41,16 @@ namespace Audio
                     break;
             }
         }
-
+        
         public void SetLevel(float sliderValue)
         {
             mixer.SetFloat(getMixerGroup(mixerGroup), Mathf.Log10(sliderValue) * 20);
             switch (getMixerGroup(mixerGroup))
             {
-                case "Music":
+                case "MusicVol":
                     music.RuntimeValue = sliderValue;
                     break;
-                case "FX":
+                case "FXVol":
                     fx.RuntimeValue = sliderValue;
                     break;
                 default:
@@ -52,7 +58,8 @@ namespace Audio
                     break;
             }
         }
-
+        
+        
         public String getMixerGroup(String mixerGroupName)
         {
             foreach (String name in mixerGroupVolumeArray)
