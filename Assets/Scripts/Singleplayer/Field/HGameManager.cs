94fd516bc223ee7b15a0f3947b101d56c42025d0
Quint Strakeljahn <q.strakeljahn@gmx.de>
2021-08-14 15:42:31 +0200

    Umstellung auf Single Spawnpoint

--- Assets/Scripts/Singleplayer/Field/HGameManager.cs
+++ Assets/Scripts/Singleplayer/Field/HGameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Singleplayer.Enemies;
 using Singleplayer.Turrets;
+using Singleplayer.Ui.Input;
 using UnityEngine;
 
 namespace Singleplayer.Field
@@ -10,21 +11,29 @@ namespace Singleplayer.Field
     public class HGameManager : MonoBehaviour
     {
         public static HGameManager instance;
+        
+        public float buildingPhaseTimer = 5;
+        public float firstBuildingPhaseTimer = 10;
+        public float betweenRoundsTimer = 10;
+        
+        //WaveSpawning
         public EnemySpawn spawnPoint;
+        public int waves;
+        public float timeBetweenMinionSpawn;
+        public float minionsPerWave;
 
         [HideInInspector] public EnemySpawn[] enemySpawns;
-        
+
+        private float _timer;
         private HCell[] spath;
         private HGrid _hexGrid;
         int input;
-    
+
         //sollte raus
         HCell start;
         HCell end;
 
         [Header("Setup Endpoint")] public int distanceFromSpawn = 5;
-
-
 
         //test für shortest path
 
@@ -39,18 +48,22 @@ namespace Singleplayer.Field
                 Debug.LogError("Es gibt mehr als einen Buildmanager in der Szene: Bitte nur eine pro Szene!");
                 return;
             }
+
             _hexGrid = GameObject.Find("HexGrid").GetComponent<HGrid>();
-            if (_hexGrid == null) throw new Exception("Kein Objekt HexGrid in der Szene gefunden oder es keine Komponente HGrid an diese! ");
+            if (_hexGrid == null)
+                throw new Exception(
+                    "Kein Objekt HexGrid in der Szene gefunden oder es keine Komponente HGrid an diese! ");
             instance = this;
         }
 
         void Start()
         {
-            TimeTickSystem.OnTick += delegate(object sender, TimeTickSystem.OnTickEventArgs args)
-            {
-            
-            };
-            
+            //Intiales setzen des Timers
+            _timer = firstBuildingPhaseTimer;
+            PlayerInputManager.Instance.SetState(new BuildState());
+
+            TimeTickSystem.OnTick += delegate(object sender, TimeTickSystem.OnTickEventArgs args) { };
+
             enemySpawns = new EnemySpawn[1];
             enemySpawns[0] = Instantiate<EnemySpawn>(spawnPoint);
             enemySpawns[0].end = _hexGrid.GetHCellByXyzCoordinates(distanceFromSpawn, 0, -distanceFromSpawn);
@@ -71,7 +84,23 @@ namespace Singleplayer.Field
 
                 _hexGrid.ShortestPathPrefabs(enemySpawn.ShortestPath(sp).ToArray());
             }
-            
+
+        }
+
+        private void Update()
+        {
+            if (_timer > 0 && !PlayerInputManager.Instance.GetState().name.Equals(StateEnum.Battle))
+            {
+                _timer -= Time.deltaTime;
+            }
+            else
+            {
+                PlayerInputManager.Instance.SetState(new BattleState());
+                SpawnEnemyWave();
+            }
+
+
+
         }
 
 
@@ -88,38 +117,39 @@ namespace Singleplayer.Field
             cooldown = false;
         }
 
+        /// <summary>
+        /// Singleplayer modus: Nur wegberechnung für einen Spawnpoint
+        /// </summary>
         public void SpawnEnemyWave()
         {
-            foreach (EnemySpawn enemySpawn in enemySpawns)
-            {
-                // könnte in der methode init werden (spath)
-                spath = enemySpawn.Solve();
-                List<HCell> sp = enemySpawn.RecPath(spath);
-                sp = enemySpawn.ShortestPath(sp);
-                
-                // es muss gecheckt werden ob die weglänge grö0er als 0 ist
-                if (sp.Count > 0)
-                    enemySpawn.SpawnEnemy(sp.ToArray(),false);
-                    // normaler modus                Nicht angreifen
-                else
-                {
-                    // attacking modus
-                    spath = enemySpawn.SolveAttack(_hexGrid.GetHCellByXyzCoordinates(0,0,0));
-                    // es wird erstmal der kürzeste weg zur base gesucht
-                    sp = enemySpawn.RecPath(spath);
-                    sp = enemySpawn.ShortestPath(sp);
-                    // danach wird am ersten turm gestoppt
-                    int towerIndex = (int) enemySpawn.TowerFinder(sp); // +1
-                    // von towerindex bis zum letzten element
-                    sp.RemoveRange(towerIndex,sp.Count-towerIndex);
-                    enemySpawn.SpawnEnemy(sp.ToArray(),true);
-                }
-                _hexGrid.ShortestPathPrefabs(sp.ToArray());
-            }
-        }
-        
+            // könnte in der methode init werden (spath)
+            spath = spawnPoint.Solve();
+            List<HCell> sp = spawnPoint.RecPath(spath);
+            sp = spawnPoint.ShortestPath(sp);
 
-        public void rerouteEnemys(HCell turretCell)
+            // es muss gecheckt werden ob die weglänge grö0er als 0 ist
+            if (sp.Count > 0)
+                spawnPoint.SpawnEnemy(sp.ToArray(), false);
+            // normaler modus                Nicht angreifen
+            else
+            {
+                // attacking modus
+                spath = spawnPoint.SolveAttack(_hexGrid.GetHCellByXyzCoordinates(0, 0, 0));
+                // es wird erstmal der kürzeste weg zur base gesucht
+                sp = spawnPoint.RecPath(spath);
+                sp = spawnPoint.ShortestPath(sp);
+                // danach wird am ersten turm gestoppt
+                int towerIndex = (int) spawnPoint.TowerFinder(sp); // +1
+                // von towerindex bis zum letzten element
+                sp.RemoveRange(towerIndex, sp.Count - towerIndex);
+                spawnPoint.SpawnEnemy(sp.ToArray(), true);
+            }
+
+            _hexGrid.ShortestPathPrefabs(sp.ToArray());
+        }
+
+
+    public void rerouteEnemys(HCell turretCell)
         {
             foreach (EnemySpawn enemySpawn in enemySpawns)
             {
---
+++ Assets/Scripts/Singleplayer/Ui/Input/StateEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Singleplayer.Ui.Input
+{
+    public static class StateEnum
+    {
+        public const string
+            Acquire = "Acquire",
+            Battle = "Battle",
+            Build = "Build";
+    }
+}
